9e3fc8645ef61e845fec75228fd6da47970b49bc
Md Munna Mia <mdmunnamia987@gmail.com>
2023-11-07 21:55:35 +0600

    some modification in module

--- Practics Projects/University management system/src/Person/Indes.js
+++ Practics Projects/University management system/src/Person/Indes.js
@@ -1,6 +1,9 @@
 const Person = requier("./Person.js"); //Person class.
-
+const Guardian = requier("./Gurdian.js");
+const UniPerson = requier("./UniPerson.js");
 
 module.exports = {
     Person,
+    Guardian,
+    UniPerson,
 }
