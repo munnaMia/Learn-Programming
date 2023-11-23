853fdd98a6075e6dc0e48a27a45290efcbe4ccc2
Md Munna Mia <mdmunnamia987@gmail.com>
2023-11-23 14:10:44 +0600

    added sharing state to REACT

---
+++ React/Learning Package 3/Shareing state/App.jsx
@@ -0,0 +1,11 @@
+import Accoroding from "./components/Accoroding";
+import Test2 from "./components/Test2";
+
+export default function App() {
+  return (
+    <>
+      <Accoroding/>
+      <Test2/>
+    </>
+  );
+}
