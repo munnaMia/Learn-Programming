ccb0314923c42a76cfa1b286ef59b7aee1db55fb
Md Munna Mia <mdmunnamia987@gmail.com>
2023-12-04 18:01:31 +0600

    adding Prime number solutions

---
+++ Problems and DSA/Mathes/Number Theory/Prime Numbers.go
@@ -0,0 +1,84 @@
+package main
+
+import "fmt"
+
+func main() {
+	var number int
+
+	fmt.Print("Enter a number: ")
+	fmt.Scan(&number)
+
+	PrimeV1(number)
+	PrimeV2(number)
+	PrimeV3(number)
+	PrimeV4(number)
+	
+}
+
+
+
+func PrimeV1(num int){
+	if num > 1 {
+		for i := num - 1; i > 1; i-- {
+			if num%i == 0 {
+				fmt.Println("This is not a Prime number")
+				return 
+			}
+		}
+	}
+	fmt.Printf("The %v Prime Number \n", num)
+}
+
+func PrimeV2(num int){
+	if num == 1 ||  num==0{
+		fmt.Printf("%v is not a prime number \n",num)
+		return
+	}else if num<0{
+		fmt.Printf("%v is a negative number \n",num)
+		return
+	}else{
+		for i := num - 1; i > 1; i-- {
+			if num%i == 0 {
+				fmt.Println("This is not a Prime number")
+				return 
+			}
+		}
+	}
+	fmt.Printf("The %v Prime Number \n", num)
+}
+
+func PrimeV3(num int){
+	if num == 1 ||  num==0 || num %2==0{
+		fmt.Printf("%v is not a prime number \n",num)
+		return
+	}else if num<0{
+		fmt.Printf("%v is a negative number \n",num)
+		return
+	}else{
+		for i := num - 1; i > 1; i-=2 {
+			if num%i == 0 {
+				fmt.Println("This is not a Prime number")
+				return 
+			}
+		}
+	}
+	fmt.Printf("The %v Prime Number \n", num)
+}
+
+func PrimeV4(num int){
+	if num == 1 ||  num==0 || num %2==0{
+		fmt.Printf("%v is not a prime number \n",num)
+		return
+	}else if num<0{
+		fmt.Printf("%v is a negative number \n",num)
+		return
+	}else{
+		for i := 2; i*i <= num; i++{
+			if num%i == 0 {
+				fmt.Println("This is not a Prime number")
+				return 
+			}
+		}
+	}
+	fmt.Printf("The %v Prime Number \n", num)
+}
